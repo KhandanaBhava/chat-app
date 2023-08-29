67a7f6a5137aaef179670b6c535431d6de117ea4
Aratrik <aratrikmukherjee2000@gmail.com>
2023-08-29 20:00:33 +0530

    initialized firebase

---
+++ src/misc/firebase.js
@@ -0,0 +1,11 @@
+import firebase from 'firebase/app';
+
+const config = {
+  apiKey: 'AIzaSyCyf6MaHe4__1hDgSOeTvTaGCKS8s-41N0',
+  authDomain: 'chat-web-app-47e8a.firebaseapp.com',
+  projectId: 'chat-web-app-47e8a',
+  storageBucket: 'chat-web-app-47e8a.appspot.com',
+  messagingSenderId: '369947816259',
+  appId: '1:369947816259:web:05d0c4ecff02254145bff6',
+};
+const app = firebase.initializeApp(config);
