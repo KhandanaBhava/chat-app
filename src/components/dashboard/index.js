fa1c15534383e645d95d434cd0197f003c9e7c9a
Aratrik <aratrikmukherjee2000@gmail.com>
2023-09-10 19:09:02 +0530

    added nickname and added reusable,editable dashboard components

--- src/components/dashboard/index.js
+++ src/components/dashboard/index.js
@@ -1,7 +1,20 @@
-import { Drawer, Button } from 'rsuite';
+import { Drawer, Button, Divider, Alert } from 'rsuite';
 import { useProfile } from '../../context/profile.context';
+import { database } from '../../misc/firebase';
+import EditableInput from '../EditableInput';
 const Dashboard = ({ onSignOut }) => {
   const { profile } = useProfile();
+  const onSave = async newData => {
+    const userNicknameRef = database
+      .ref(`/profiles/${profile.uid}`)
+      .child('name');
+    try {
+      await userNicknameRef.set(newData);
+      Alert.success('Nickname has been updated', 4000);
+    } catch (err) {
+      Alert.error(err.message, 4000);
+    }
+  };
   return (
     <>
       <Drawer.Header>
@@ -9,6 +22,13 @@ const Dashboard = ({ onSignOut }) => {
       </Drawer.Header>
       <Drawer.Body>
         <h3>Hey,{profile.name}</h3>
+        <Divider />
+        <EditableInput
+          name="nickname"
+          initialValue={profile.name}
+          onSave={onSave}
+          label={<h6 className="mb-2">Nickname</h6>}
+        />
       </Drawer.Body>
       <Drawer.Footer>
         <Button block color="red" onClick={onSignOut}>
